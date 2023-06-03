51c95e1b9b0cd136e161db09bde806fc681929b3
Amin Yarits Firdaus <amyar32@gmail.com>
2023-06-03 18:21:02 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Swiper
+    class="max-h-screen"
+    :modules="[SwiperPagination, SwiperMousewheel]"
+    :slides-per-view="1"
+    :direction="'vertical'"
+    pagination
+    mousewheel
+  >
+    <SwiperSlide class="bg-green-500">
+      <strong>Page 1</strong>
+    </SwiperSlide>
+    <SwiperSlide class="bg-red-500">
+      <strong>Page 2</strong>
+    </SwiperSlide>
+  </Swiper>
+</template>
